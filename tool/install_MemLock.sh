3927751d4b299842149b64e627a08642df665c21
wcventure <wencheng888888@163.com>
2019-12-30 16:21:25 +0800

    install_MemLock.sh

--- tool/install_MemLock.sh
+++ tool/install_MemLock.sh
@@ -5,16 +5,20 @@ ROOT_DIR=$(dirname $(dirname $BIN_PATH))
 set -euxo pipefail
 
 if ! [ -d "${ROOT_DIR}/clang+llvm"  ]; then
-    ${ROOT_DIR}/tool/install_llvm.sh
-    export PATH=${ROOT_DIR}/clang+llvm/bin:$PATH
-    export LD_LIBRARY_PATH=${ROOT_DIR}/clang+llvm/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-    export CC=clang
-    export CXX=clang++
+	${ROOT_DIR}/tool/install_llvm.sh
 fi
+
+export PATH=${ROOT_DIR}/clang+llvm/bin:$PATH
+export LD_LIBRARY_PATH=${ROOT_DIR}/clang+llvm/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+export CC=clang
+export CXX=clang++
 
 PREFIX=${PREFIX:-${ROOT_DIR}/tool/MemLock/build}
 
+
+
 cd ${ROOT_DIR}/tool/MemLock
+make clean
 make
 cd ${ROOT_DIR}/tool/MemLock/llvm_mode
 make
@@ -22,8 +26,6 @@ cd ${ROOT_DIR}/tool/MemLock
 make install
 
 cd ${ROOT_DIR}/tool/AFL-2.52b
+make clean
 make
-cd ${ROOT_DIR}/tool/AFL-2.52b/llvm_mode
-make
-cd ${ROOT_DIR}/tool/AFL-2.52b
 make install
