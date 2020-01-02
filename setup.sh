8c1db4a2356e1c9fac80b558742427c161e8cdf3
wcventure <wencheng888888@163.com>
2020-01-02 17:10:33 +0800

    sudo

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 BIN_PATH=$(readlink -f "$0")
 ROOT_DIR=$(dirname $BIN_PATH)
 
+echo core|sudo tee /proc/sys/kernel/core_pattern
+echo performance|sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
 ${ROOT_DIR}/tool/install_llvm.sh
 ${ROOT_DIR}/tool/install_MemLock.sh
 ${ROOT_DIR}/evaluation/BUILD/build_cxxfilt.sh
